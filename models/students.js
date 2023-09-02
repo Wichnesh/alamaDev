93e197aeebd444860b4b3cc6b031a598721df34f
Wichnesh <112880628+Wichnesh@users.noreply.github.com>
2023-09-02 12:34:32 +0530

    Add files via upload

---
+++ models/students.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  studentID: {
+    type: Number,
+    required: true,
+  },
+  enrollDate: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  studentName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  mobileNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  franchise: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    required: true,
+  },
+  items: {
+    pencil: {
+      type: String,
+    },
+    bag: {
+      type: String,
+    },
+    abacus: {
+      type: String,
+    },
+    listeningAbility: {
+      type: String,
+    },
+    progressCard: {
+      type: String,
+    },
+    tShirt: {
+      type: String,
+    },
+  },
+  program: {
+    type: String,
+    required: true,
+  },
+  cost: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Studentlist = (module.exports = mongoose.model(
+  "Studentlist",
+  studentSchema
+));
