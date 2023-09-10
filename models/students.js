5ae94c03deff873eb902ce9246d81ce310195c3d
Wichnesh <112880628+Wichnesh@users.noreply.github.com>
2023-09-10 10:55:07 +0530

    Revert

--- models/students.js
+++ models/students.js
@@ -68,15 +68,10 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  levelOrders:{
-    level:{
-      type:String
-    },
-    date:{
-      type:Date,
-      default: new Date().toLocaleDateString("en-US"),
-    }
-  }
+  // levelOrders: [{
+  //       level: String,
+  //       date: Date
+  //   }]
 });
 
 const Studentlist = (module.exports = mongoose.model(
