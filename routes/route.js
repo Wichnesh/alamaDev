aaaf0993a640a8a42b03759d95e2f5df05f7f1ec
Wichnesh <112880628+Wichnesh@users.noreply.github.com>
2023-12-22 06:30:05 -0800

    delete empty report

--- routes/route.js
+++ routes/route.js
@@ -860,6 +860,9 @@ route.post("/data", async (req, res) => {
       }
     }
     mergedArr[i]["totalItems"] = totalItems;
+    if((mergedArr[i]["totalItems"].length < 0) && (mergedArr[i]["enrolledStudents"].length < 0)){
+        mergedArr[i].splice(0,1);
+    }
     delete mergedArr[i]["count"];
     delete mergedArr[i]["orderCounts"];
   }
